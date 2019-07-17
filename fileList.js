14820ceb393b54f7f9633c5835d07656ef5579b6
edongyun <ldy@gsm.hs.kr>
2019-07-17 09:06:27 +0900

    vs code

--- fileList.js
+++ fileList.js
@@ -1,8 +1,18 @@
 var fs = require('fs');
+var path = require('path');
 
-fs.readdir('./Chapter02/ModuleSystem', function(err, files){
+var filesPath = path.join(__dirname, 'Chapter02', 'ModuleSystem');
+
+fs.readdir(filesPath, function(err, files){
     console.log(files);
     files.forEach(function(value){
-        console.log(value);
+        fs.stat(path.join(filesPath, value), function(err, stats){
+            if(err){
+                console.log(err.message)
+            }else{
+                if(stats.isFile())
+                    console.log(value);
+            }
+        })
     })
 })
