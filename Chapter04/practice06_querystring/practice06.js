bc005e0835c40669f929d16b3d4f39abd9f35c3b
goodluck3586 <52552928+goodluck3586@users.noreply.github.com>
2020-10-20 10:41:05 +0900

    ㅎㅎ

--- Chapter04/practice06_querystring/practice06.js
+++ Chapter04/practice06_querystring/practice06.js
@@ -2,35 +2,44 @@ var http = require('http');
 var url = require('url');
 var querystring = require('querystring');
 
-//#region 1. get 방식으로 넘어온 두 값(start, end)을 활용하여 두 값 사이의 합계 구하기
-var server = http.createServer(function(req, res){
-  const parsedURL = url.parse(req.url); // <1> url을 parsing하여 객체로 저장한다.
-  console.log('parsedURL.query : ', parsedURL.query);  // start=1&end=100
-  const query = querystring.parse(parsedURL.query); // <2> parsedURL을 parsing하여 querystring을 추출한 객체를 저장한다.
-  console.log('query : ', query);  // { start: '1', end: '100' }
+//#region 1. get 방식으로 넘어온 두 값(num1, num2)을 활용하여 두 값 사이의 합계 구하기
+// var server = http.createServer(function(req, res){
+//   console.log('req.url', req.url)
+//   if(req.url === '/favicon.ico'){
+//     res.end()
+//     return
+//   }
 
-  // start와 end에 합계를 구할 범위의 숫자를 정수로 변환하여 저장한다.
-  let start = parseInt(query.start);
-  let end = parseInt(query.end); 
-  console.log(`start : ${start}, end : ${end}`);
+//   // <1> url을 parsing하여 객체로 저장
+//   const parsedURL = url.parse(req.url); 
+//   console.log('parsedURL.query : ', parsedURL.query);  // num1=1&num2=100
 
-  // start와 end 변수가 비어있으면 404 에러 코드 전송
-  if( !start || !end){
-    res.statusCode = 400;  // 400 Bad Request
-    res.end('Wrong Parameter');
-  }else{
-    var sum = 0;
-    for(var i=start; i<=end; i++){
-      sum += i;
-    }
-    res.end(`<h1>${sum}</h1>`);  // <4> start부터 end까지의 합계를 <h1>태그로 감싸서 출력한다. 
-  }
-}).listen(8080, function(){
-  console.log('8080 포트에서 대기중');
-});
+//   // <2> parsing된 url에서 querystring을 추출하여 객체로 저장
+//   const qs = querystring.parse(parsedURL.query); 
+//   console.log('querystring : ', qs);  // { num1: '1', num2: '100' }
+
+//   // <3> 변수 num1, num2에 querystring의 num1, num2값을 정수로 변환하여 저장
+//   let num1 = parseInt(qs.num1);
+//   let num2 = parseInt(qs.num2); 
+//   console.log(`num1 : ${num1}, num2 : ${num2}`);
+
+//   // <4> num1, num2에 잘못된 값이 들어있으면 400 Bad Request 에러 코드 전송
+//   if( Number.isNaN(num1) || Number.isNaN(num2) ){
+//     res.statusCode = 400;  // 400 Bad Request
+//     res.end('<h1>Bad Request</h1>');
+//   }else{
+//     var sum = 0;
+//     for(var i=num1; i<=num2; i++){
+//       sum += i;
+//     }
+//     res.end(`<h1>${sum}</h1>`);  // <4> num1부터 num2까지의 합계를 <h1>태그로 감싸서 출력한다. 
+//   }
+// }).listen(8080, function(){
+//   console.log('8080 포트에서 대기중');
+// });
 //#endregion
 
-//#region 2. post 방식으로 넘어온 두 값(start, end)을 활용하여 두 값 사이의 합계 구하기
+//#region 2. post 방식으로 넘어온 두 값(num1, num2)을 활용하여 두 값 사이의 합계 구하기
 var server = http.createServer(function(req, res){
   if(req.method.toLowerCase() === 'post'){  // 메소드가 post 방식인가?
     var body = '';
@@ -41,16 +50,16 @@ var server = http.createServer(function(req, res){
 
     req.on('end', function(){
       var data = querystring.parse(body);
-      let start = parseInt(data.start);
-      let end = parseInt(data.end); 
-      console.log(`start : ${start}, end : ${end}`);
+      let num1 = parseInt(data.num1);
+      let num2 = parseInt(data.num2); 
+      console.log(`num1 : ${num1}, num2 : ${num2}`);
 
-      if( !start || !end){
+      if( Number.isNaN(num1) || Number.isNaN(num2) ){
         res.statusCode = 400;  // 400 Bad Request
-        res.end('Wrong Parameter');
+        res.end('<h1>Bad Request</h1>');
       }else{
         var sum = 0;
-        for(var i=start; i<=end; i++){
+        for(var i=num1; i<=num2; i++){
           sum += i;
         }
         res.end(`<h1>${sum}</h1>`);
