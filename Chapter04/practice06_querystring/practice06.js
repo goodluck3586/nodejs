1374e3711d7ccc69128e1fb97c63af17c3afffda
DESKTOP-BB105KM\user <goodluck3586@gsm.hs.kr>
2020-11-05 10:28:44 +0900

    ㅎㅎ

--- Chapter04/practice06_querystring/practice06.js
+++ Chapter04/practice06_querystring/practice06.js
@@ -3,40 +3,40 @@ var url = require('url');
 var querystring = require('querystring');
 
 //#region 1. get 방식으로 넘어온 두 값(num1, num2)을 활용하여 두 값 사이의 합계 구하기
-// var server = http.createServer(function(req, res){
-//   console.log('req.url', req.url)
-//   if(req.url === '/favicon.ico'){
-//     res.end()
-//     return
-//   }
+var server = http.createServer(function(req, res){
+  console.log('req.url', req.url)
+  if(req.url === '/favicon.ico'){
+    res.end()
+    return
+  }
 
-//   // <1> url을 parsing하여 객체로 저장
-//   const parsedURL = url.parse(req.url); 
-//   console.log('parsedURL.query : ', parsedURL.query);  // num1=1&num2=100
+  // <1> url을 parsing하여 객체로 저장
+  const parsedURL = url.parse(req.url); 
+  console.log('parsedURL.query : ', parsedURL.query);  // num1=1&num2=100
 
-//   // <2> parsing된 url에서 querystring을 추출하여 객체로 저장
-//   const qs = querystring.parse(parsedURL.query); 
-//   console.log('querystring : ', qs);  // { num1: '1', num2: '100' }
+  // <2> parsing된 url에서 querystring을 추출하여 객체로 저장
+  const qs = querystring.parse(parsedURL.query); 
+  console.log('querystring : ', qs);  // { num1: '1', num2: '100' }
 
-//   // <3> 변수 num1, num2에 querystring의 num1, num2값을 정수로 변환하여 저장
-//   let num1 = parseInt(qs.num1);
-//   let num2 = parseInt(qs.num2); 
-//   console.log(`num1 : ${num1}, num2 : ${num2}`);
+  // <3> 변수 num1, num2에 querystring의 num1, num2값을 정수로 변환하여 저장
+  let num1 = parseInt(qs.num1);
+  let num2 = parseInt(qs.num2); 
+  console.log(`num1 : ${num1}, num2 : ${num2}`);
 
-//   // <4> num1, num2에 잘못된 값이 들어있으면 400 Bad Request 에러 코드 전송
-//   if( Number.isNaN(num1) || Number.isNaN(num2) ){
-//     res.statusCode = 400;  // 400 Bad Request
-//     res.end('<h1>Bad Request</h1>');
-//   }else{
-//     var sum = 0;
-//     for(var i=num1; i<=num2; i++){
-//       sum += i;
-//     }
-//     res.end(`<h1>${sum}</h1>`);  // <4> num1부터 num2까지의 합계를 <h1>태그로 감싸서 출력한다. 
-//   }
-// }).listen(8080, function(){
-//   console.log('8080 포트에서 대기중');
-// });
+  // <4> num1, num2에 잘못된 값이 들어있으면 400 Bad Request 에러 코드 전송
+  if( Number.isNaN(num1) || Number.isNaN(num2) ){
+    res.statusCode = 400;  // 400 Bad Request
+    res.end('<h1>Bad Request</h1>');
+  }else{
+    var sum = 0;
+    for(var i=num1; i<=num2; i++){
+      sum += i;
+    }
+    res.end(`<h1>${sum}</h1>`);  // <4> num1부터 num2까지의 합계를 <h1>태그로 감싸서 출력한다. 
+  }
+}).listen(8080, function(){
+  console.log('8080 포트에서 대기중');
+});
 //#endregion
 
 //#region 2. post 방식으로 넘어온 두 값(num1, num2)을 활용하여 두 값 사이의 합계 구하기
--- Chapter05/hello-express/app.js
+++ Chapter05/hello-express/app.js
@@ -1,61 +1,62 @@
 var express = require('express');
-var app = express();
+var app = express();                // app은 express의 인스턴스
+const port = 3000
 
-app.use(express.static('public'));
+//#region 1. Hello world 예제
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World!</h1>')
+// })
 
-app.get('/zzz', function(req, res){
-    console.log(`req.url: ${req.url}`);
-    // res.redirect('./public/index.html')
-    res.redirect('/index.html')
-});
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+//#endregion
 
-app.get('/hi', function(req, res){
-    res.send('<h1>Hi Express</h1>');
-});
+//#region 2. 기본 라우팅
+// 라우팅은 URL과 HTTP요청메소드(GET, POST 등)인 특정 엔드포인트에 대한 클라이언트 요청에 대한 응답
+// 각 라우트는 하나 이상의 핸들러 함수를 가질 수 있으며, 이러한 함수는 라우트가 일치할 때 실행된다.
+app.get('/', (req, res) => res.send('<h1>Get request</h1>'))
+app.post('/', (req, res) => res.send('<h1>Post request</h1>'))
 
-app.get('/iu', function(req, res){
-    res.sendFile('iu.png')
-});
+app.get('/book', (req, res) => res.send('<h1>Get a book</h1>'))
+app.post('/book', (req, res) => res.send('<h1>Add a book</h1>'))
+app.put('/book', (req, res) => res.send('<h1>Update a book</h1>'))
+app.delete('/book', (req, res) => res.send('<h1>Delete a book</h1>'))
 
-app.listen(8080, function(){
-    console.log('8080 포트에서 대기중');
-});
+// app.route()를 이용한 라우트 체인 => 중복과 오타 감소
+app.route('/user')
+    .get((req, res) => res.send('<h1>Get a user</h1>'))
+    .post((req, res) => res.send('<h1>Add a user</h1>'))
+    .put((req, res) => res.send('<h1>Update a user</h1>'))
+    .delete((req, res) => res.send('<h1>Delete a user</h1>'))
 
-// // 2. 라우팅 처리
-// app.get('/', function(req, res){
-//     res.send('<h1>Get request</h1>');
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+//#endregion
+
+//#region 3. Express에서 정적 파일 제공
+// app.use(express.static('public'));
+
+// app.get('/zzz', function(req, res){
+//     console.log(`req.url: ${req.url}`);
+//     // res.redirect('./public/index.html')
+//     res.redirect('/index.html')
 // });
-// app.post('/', function(req, res){
-//     res.send('<h1>Post request</h1>');
+
+// app.get('/hi', function(req, res){
+//     res.send('<h1>Hi Express</h1>');
 // });
 
-// // app.get('/book', function(req, res){
-// //     res.send('Get a book');
-// // });
-// // app.post('/book', function(req, res){
-// //     res.send('Add a book');
-// // });
-// // app.put('/book', function(req, res){
-// //     res.send('Update a book');
-// // });
-// // app.delete('/book', function(req, res){
-// //     res.send('Delete a book');
-// // });
+// app.get('/iu', function(req, res){
+//     res.sendFile('iu.png')
+// });
 
-// // app.route() 라우트 체인
-// app.route('/book')
-//     .get(function(req, res){
-//         res.send('Get a book');
-//     })
-//     .post(function(req, res){
-//         res.send('Add a book');
-//     })
-//     .put(function(req, res){
-//         res.send('Update a book');
-//     })
-//     .delete(function(req, res){
-//         res.send('Delete a book');
-//     })
+// app.listen(8080, function(){
+//     console.log('8080 포트에서 대기중');
+// });
+//#endregion
+
 
 // // 3. 미들웨어(사용자 정의)
 // // Express는 웹 서비스를 위한 기본적인 기능과 구조를 제공한다.
