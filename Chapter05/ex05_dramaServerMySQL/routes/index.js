60584f49cb4dcc6fc44b3476fa0a062f95ce43e5
goodluck3586 <52552928+goodluck3586@users.noreply.github.com>
2020-11-16 00:59:52 +0900

    2020-11-16 수업준비

--- Chapter05/ex05_dramaServerMySQL/routes/index.js
+++ Chapter05/ex05_dramaServerMySQL/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var model = require('../models/drama');
+var model = require('../models/dramaDAO');
 
 router.get('/', function(req, res) {
   model.selectDrama((results)=>{
@@ -11,12 +11,28 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res){
   if(req.body.title && req.body.actor){
-    model.insertDrama(req.body, (results)=>{
+    model.insertDrama(req.body, ()=>{
       res.redirect('/');
     })
   }else{
-    res.render('dramaList', { title: 'My Favorite Drama List', list: results });
+    res.redirect('/');
   }
+})
+
+router.post('/update/:id', function(req, res){
+  //res.send(`/update req.params.id: ${req.params.id}`);
+  if(req.body.title && req.body.actor){
+    model.updateDrama(req.params.id, req.body, ()=>{
+      res.redirect('/');
+    })
+  }else{
+    res.redirect('/');
+  }
+})
+
+router.get('/delete/:id', function(req, res){
+  //res.send(`/delete req.params.id: ${req.params.id}`);
+  model.deleteDrama(req.params.id, ()=>res.redirect('/'))
 })
 
 module.exports = router;
