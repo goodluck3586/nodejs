50acbfe37f6003a3b2093a8a6d6c05676c4f7c24
goodluck3586 <52552928+goodluck3586@users.noreply.github.com>
2020-12-28 14:17:15 +0900

    gg

--- Chapter07/ex05_loginUsingMysqlSession/routes/drama.js
+++ Chapter07/ex05_loginUsingMysqlSession/routes/drama.js
@@ -2,32 +2,28 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/dramaDAO');
 
-router.get('/', (req, res)=>{
-  model.selectDrama()
-  .then((results)=>res.render(
-    'dramaList', 
-    { title: 'My Favorite Drama List', list: results, isLogin: req.session.isLogin }))
-  .catch(console.log)
+router.get('/', async (req, res)=>{
+  let results = await model.selectDrama()
+  res.render('dramaList', { title: 'My Favorite Drama List', list: results, isLogin: req.session.isLogin })
 })
 
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
   if(req.body.title && req.body.actor){
-    model.insertDrama(req.body)
-      .finally(res.redirect('/drama'))
+    await model.insertDrama(req.body)
+    res.redirect('/drama')
   }
 })
 
-router.post('/update/:id', function(req, res){
+router.post('/update/:id', async (req, res)=>{
   if(req.body.title && req.body.actor){
-    model.updateDrama(req.params.id, req.body)
-      .finally(res.redirect('/drama'))
-  }else{
-    res.redirect('/drama');
+    await model.updateDrama(req.params.id, req.body)
   }
+  res.redirect('/drama')
 })
 
-router.get('/delete/:id', function(req, res){
-  model.deleteDrama(req.params.id).finally(res.redirect('/drama'))
+router.get('/delete/:id', async (req, res)=>{
+  await model.deleteDrama(req.params.id)
+  res.redirect('/drama')
 })
 
 module.exports = router;
--- Chapter07/ex05_loginUsingMysqlSession/routes/users.js
+++ Chapter07/ex05_loginUsingMysqlSession/routes/users.js
@@ -2,19 +2,16 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/usersDAO');
 
-router.post('/login', (req, res)=>{
+router.post('/login', async (req, res)=>{
   if(req.body.email && req.body.pwd){
-    model.selectUser(req.body.email)
-      .then((results)=>{
-        if(req.body.email === results[0].email && req.body.pwd === results[0].pwd){
-          req.session.isLogin = true;
-          req.session.userEmail = req.body.email;
-          res.redirect('/')
-        }else{
-          res.send('<h1>로그인 실패</h1>')
-        }
-      })
-      .catch(console.log)
+    let results = await model.selectUser(req.body.email)
+    if(req.body.email === results[0].email && req.body.pwd === results[0].pwd){
+      req.session.isLogin = true;
+      req.session.userEmail = req.body.email;
+      res.redirect('/')
+    }else{
+      res.send('<h1>로그인 실패</h1>')
+    }
   }
 })
 
